0fa93cbdc678e613b94ee40a3a641f5d754dd97e
kit10000000 <ekaterina0196@yandex.ru>
2015-11-30 21:28:34 +0300

    1-st change
    Добавила создание  клиентского канала(не общего!!!)

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -6,19 +6,19 @@
 #include <thread>
 #include "windows.h"
 #include <iostream>
-
+HANDLE clientPipe;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	HANDLE hPipe = CreateNamedPipe("\\\\.\\pipe\\MyPipe", PIPE_ACCESS_INBOUND, PIPE_TYPE_MESSAGE, PIPE_UNLIMITED_INSTANCES, 255, 255, NMPWAIT_USE_DEFAULT_WAIT, NULL);
 	ConnectNamedPipe(hPipe, NULL);
-	std::string clientPipeName[512];
+	char UsernamePipe[512];
 	DWORD cbRead;
-	
 	while (true)
 	{
-		if (ReadFile(hPipe, clientPipeName, 512, &cbRead, NULL))
+		if (ReadFile(hPipe, UsernamePipe, 512, &cbRead, NULL))
 		{
-			auto fullName = "\\\\.\\pipe\\MyPipe" + clientPipeName;
+			auto fullName = "\\\\.\\pipe\\MyPipe" + (char const)UsernamePipe;
+			clientPipe = CreateNamedPipe(fullName, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE, PIPE_UNLIMITED_INSTANCES, 255, 255, NMPWAIT_USE_DEFAULT_WAIT, nullptr);
 		}
 	}
 	DisconnectNamedPipe(hPipe);
